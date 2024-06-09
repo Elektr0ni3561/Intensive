683d7afdc65bc4249e3252ee13939881a4ba4e70
Elektr0ni3561 <ShokolaDka3561@yandex.ru>
2024-06-09 17:02:54 +0300

    master Good changes!

--- src/main/java/ru/elektr0ni3561/homework8/task1/Main1.java
+++ src/main/java/ru/elektr0ni3561/homework8/task1/Main1.java
@@ -7,7 +7,7 @@ public class Main1 {
         System.out.println("Hello world!");
 
         for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+            System.out.println("I equals " + i + "!");
         }
 
         System.out.println("Bye world!");
